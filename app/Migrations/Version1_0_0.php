da49e5834fcd2f638acf6bb61d8d9321a5869f59
mario <mario@digitalstate.ca>
2017-06-17 18:42:31 -0400

    Create migration file

---
+++ app/Migrations/Version1_0_0.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Class Version1_0_0
+ */
+class Version1_0_0 extends AbstractMigration
+{
+    /**
+     * Up
+     *
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE ds_asset (id INT AUTO_INCREMENT NOT NULL, uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', `owner` VARCHAR(255) DEFAULT NULL, owner_uuid CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\', identity VARCHAR(255) DEFAULT NULL, identity_uuid CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\', deleted_at DATETIME DEFAULT NULL, created_at DATETIME DEFAULT NULL, updated_at DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_D556ACFDD17F50A6 (uuid), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE ds_asset_association (id INT AUTO_INCREMENT NOT NULL, asset_id INT DEFAULT NULL, uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', entity VARCHAR(255) NOT NULL, entity_uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', `owner` VARCHAR(255) NOT NULL, owner_uuid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', deleted_at DATETIME DEFAULT NULL, created_at DATETIME DEFAULT NULL, updated_at DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_68E7A63ED17F50A6 (uuid), INDEX IDX_68E7A63E5DA1941 (asset_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE ds_asset_trans (id INT AUTO_INCREMENT NOT NULL, translatable_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, deleted_at DATETIME DEFAULT NULL, created_at DATETIME DEFAULT NULL, updated_at DATETIME DEFAULT NULL, locale VARCHAR(255) NOT NULL, INDEX IDX_83E62DD42C2AC5D3 (translatable_id), UNIQUE INDEX ds_asset_trans_unique_translation (translatable_id, locale), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE ds_asset_association ADD CONSTRAINT FK_68E7A63E5DA1941 FOREIGN KEY (asset_id) REFERENCES ds_asset (id)');
+        $this->addSql('ALTER TABLE ds_asset_trans ADD CONSTRAINT FK_83E62DD42C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES ds_asset (id) ON DELETE CASCADE');
+    }
+
+    /**
+     * Down
+     *
+     * @param \Doctrine\DBAL\Schema\Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE ds_asset_association DROP FOREIGN KEY FK_68E7A63E5DA1941');
+        $this->addSql('ALTER TABLE ds_asset_trans DROP FOREIGN KEY FK_83E62DD42C2AC5D3');
+        $this->addSql('DROP TABLE ds_asset');
+        $this->addSql('DROP TABLE ds_asset_association');
+        $this->addSql('DROP TABLE ds_asset_trans');
+    }
+}
