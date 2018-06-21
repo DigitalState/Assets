aa11bad0b474f937e1d3a748c2ee40db8904e27d
mario <mario@digitalstate.ca>
2018-06-21 13:02:10 -0400

    Restructure based on new core

--- src/AppBundle/Fixtures/Assets.php
+++ src/AppBundle/Fixtures/Assets.php
@@ -15,7 +15,7 @@ class Assets extends AssetFixture implements OrderedFixtureInterface
      */
     public function getOrder()
     {
-        return 10;
+        return 20;
     }
 
     /**
--- src/AppBundle/Migrations/Version1_0_0.php
+++ src/AppBundle/Migrations/Version1_0_0.php
@@ -21,11 +21,12 @@ class Version1_0_0 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        // Schema
+        // Entity schema
         $this->addSql('CREATE SEQUENCE ds_config_id_seq INCREMENT BY 1 MINVALUE 1 START 9');
         $this->addSql('CREATE SEQUENCE ds_parameter_id_seq INCREMENT BY 1 MINVALUE 1 START 2');
         $this->addSql('CREATE SEQUENCE ds_access_id_seq INCREMENT BY 1 MINVALUE 1 START 3');
         $this->addSql('CREATE SEQUENCE ds_access_permission_id_seq INCREMENT BY 1 MINVALUE 1 START 7');
+        $this->addSql('CREATE SEQUENCE ds_tenant_id_seq INCREMENT BY 1 MINVALUE 1 START 2');
         $this->addSql('CREATE SEQUENCE app_asset_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE app_asset_association_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE app_asset_trans_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
@@ -38,6 +39,8 @@ class Version1_0_0 extends AbstractMigration
         $this->addSql('CREATE UNIQUE INDEX UNIQ_A76F41DCD17F50A6 ON ds_access (uuid)');
         $this->addSql('CREATE TABLE ds_access_permission (id INT NOT NULL, access_id INT DEFAULT NULL, scope VARCHAR(255) DEFAULT NULL, entity VARCHAR(255) DEFAULT NULL, entity_uuid UUID DEFAULT NULL, "key" VARCHAR(255) NOT NULL, attributes JSON NOT NULL, tenant UUID NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_D46DD4D04FEA67CF ON ds_access_permission (access_id)');
+        $this->addSql('CREATE TABLE ds_tenant (id INT NOT NULL, uuid UUID NOT NULL, data JSON NOT NULL, version INT DEFAULT 1 NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_EF5FAEEAD17F50A6 ON ds_tenant (uuid)');
         $this->addSql('CREATE TABLE app_asset (id INT NOT NULL, uuid UUID NOT NULL, "owner" VARCHAR(255) DEFAULT NULL, owner_uuid UUID DEFAULT NULL, identity VARCHAR(255) DEFAULT NULL, identity_uuid UUID DEFAULT NULL, data JSON NOT NULL, version INT DEFAULT 1 NOT NULL, tenant UUID NOT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_D47CD791D17F50A6 ON app_asset (uuid)');
         $this->addSql('CREATE TABLE app_asset_association (id INT NOT NULL, asset_id INT DEFAULT NULL, uuid UUID NOT NULL, entity VARCHAR(255) NOT NULL, entity_uuid UUID NOT NULL, "owner" VARCHAR(255) NOT NULL, owner_uuid UUID NOT NULL, version INT DEFAULT 1 NOT NULL, tenant UUID NOT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
@@ -50,12 +53,27 @@ class Version1_0_0 extends AbstractMigration
         $this->addSql('ALTER TABLE app_asset_association ADD CONSTRAINT FK_D7B8D55A5DA1941 FOREIGN KEY (asset_id) REFERENCES app_asset (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE app_asset_trans ADD CONSTRAINT FK_BC9F16462C2AC5D3 FOREIGN KEY (translatable_id) REFERENCES app_asset (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
 
+        // Custom schema
         $this->addSql('CREATE TABLE ds_session (id VARCHAR(128) NOT NULL PRIMARY KEY, data BYTEA NOT NULL, time INTEGER NOT NULL, lifetime INTEGER NOT NULL)');
 
         // Data
         $yml = file_get_contents('/srv/api-platform/src/AppBundle/Resources/migrations/1_0_0.yml');
         $data = Yaml::parse($yml);
 
+        $this->addSql('
+            INSERT INTO 
+                ds_parameter (id, key, value, enabled)
+            VALUES 
+                (1, \'ds_tenant.tenant.default\', \'"'.$data['tenant']['uuid'].'"\', true);
+        ');
+
+        $this->addSql('
+            INSERT INTO 
+                ds_tenant (id, uuid, data, created_at, updated_at)
+            VALUES 
+                (1, \''.$data['tenant']['uuid'].'\', \'{}\', now(), now());
+        ');
+        
         $this->addSql('
             INSERT INTO 
                 ds_config (id, uuid, owner, owner_uuid, key, value, enabled, version, tenant, created_at, updated_at)
@@ -68,13 +86,6 @@ class Version1_0_0 extends AbstractMigration
                 (6, \''.Uuid::uuid4()->toString().'\', \'BusinessUnit\', \''.$data['business_unit']['administration']['uuid'].'\', \'ds_api.user.identity.type\', \'"System"\', true, 1, \''.$data['tenant']['uuid'].'\', now(), now()),
                 (7, \''.Uuid::uuid4()->toString().'\', \'BusinessUnit\', \''.$data['business_unit']['administration']['uuid'].'\', \'ds_api.user.identity.uuid\', \'"'.$data['identity']['system']['uuid'].'"\', true, 1, \''.$data['tenant']['uuid'].'\', now(), now()),
                 (8, \''.Uuid::uuid4()->toString().'\', \'BusinessUnit\', \''.$data['business_unit']['administration']['uuid'].'\', \'ds_api.user.tenant\', \'"'.$data['tenant']['uuid'].'"\', true, 1, \''.$data['tenant']['uuid'].'\', now(), now());
-        ');
-
-        $this->addSql('
-            INSERT INTO 
-                ds_parameter (id, key, value, enabled)
-            VALUES 
-                (1, \'ds_tenant.tenant.default\', \'"'.$data['tenant']['uuid'].'"\', true);
         ');
 
         $this->addSql('
@@ -107,7 +118,7 @@ class Version1_0_0 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        // Schema
+        // Entity schema
         $this->addSql('ALTER TABLE ds_access_permission DROP CONSTRAINT FK_D46DD4D04FEA67CF');
         $this->addSql('ALTER TABLE app_asset_association DROP CONSTRAINT FK_D7B8D55A5DA1941');
         $this->addSql('ALTER TABLE app_asset_trans DROP CONSTRAINT FK_BC9F16462C2AC5D3');
@@ -115,6 +126,7 @@ class Version1_0_0 extends AbstractMigration
         $this->addSql('DROP SEQUENCE ds_parameter_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE ds_access_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE ds_access_permission_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE ds_tenant_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE app_asset_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE app_asset_association_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE app_asset_trans_id_seq CASCADE');
@@ -122,10 +134,12 @@ class Version1_0_0 extends AbstractMigration
         $this->addSql('DROP TABLE ds_parameter');
         $this->addSql('DROP TABLE ds_access');
         $this->addSql('DROP TABLE ds_access_permission');
+        $this->addSql('DROP TABLE ds_tenant');
         $this->addSql('DROP TABLE app_asset');
         $this->addSql('DROP TABLE app_asset_association');
         $this->addSql('DROP TABLE app_asset_trans');
 
+        // Custom schema
         $this->addSql('DROP TABLE ds_session');
     }
 }
